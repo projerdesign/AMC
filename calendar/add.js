cfb149b4bc591b08a00cc6f14b0e1abe9b869a1b
Projer (Oscar) <projerdj@gmail.com>
2014-08-08 17:55:46 -0500

    addcalentar_test1

---
+++ calendar/add.js
@@ -0,0 +1,58 @@
+// prep some variables
+  var startDate = new Date(2014,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
+  var endDate = new Date(2014,2,15,19,30,0,0,0);
+  var title = "My nice event";
+  var location = "Home";
+  var notes = "Some notes about this event.";
+  var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+  var error = function(message) { alert("Error: " + message); };
+
+  // create a calendar (iOS only for now)
+  window.plugins.calendar.createCalendar(calendarName,success,error);
+  // if you want to create a calendar with a specific color, pass in a JS object like this:
+  var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
+  createCalOptions.calendarName = "My Cal Name";
+  createCalOptions.calendarColor = "#FF0000"; // an optional hex color (with the # char), default is null, so the OS picks a color
+  window.plugins.calendar.createCalendar(createCalOptions,success,error);
+
+  // delete a calendar (iOS only for now)
+  window.plugins.calendar.deleteCalendar(calendarName,success,error);
+
+  // create an event silently (on Android < 4 an interactive dialog is shown)
+  window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
+
+  // create an event silently (on Android < 4 an interactive dialog is shown which doesn't use this options) with options:
+  var calOptions = window.plugins.calendar.getCalendarOptions(); // grab the defaults
+  calOptions.firstReminderMinutes = 120; // default is 60, pass in null for no reminder (alarm)
+  calOptions.secondReminderMinutes = 5;
+
+  // Added these options in version 4.2.4:
+  calOptions.recurrence = "monthly"; // supported are: daily, weekly, monthly, yearly
+  calOptions.recurrenceEndDate = new Date(2015,6,1,0,0,0,0,0); // leave null to add events into infinity and beyond
+  calOptions.calendarName = "MyCreatedCalendar"; // iOS only
+  window.plugins.calendar.createEventWithOptions(title,location,notes,startDate,endDate,calOptions,success,error);
+
+  // create an event interactively (only supported on Android)
+  window.plugins.calendar.createEventInteractively(title,location,notes,startDate,endDate,success,error);
+
+  // create an event in a named calendar (iOS only for now)
+  window.plugins.calendar.createEventInNamedCalendar(title,location,notes,startDate,endDate,calendarName,success,error);
+
+  // find events
+  window.plugins.calendar.findEvent(title,location,notes,startDate,endDate,success,error);
+
+  // list all events in a date range (only supported on Android for now)
+  window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
+
+  // list all calendar names - returns this JS Object to the success callback: [{"id":"1", "name":"first"}, ..]
+  window.plugins.calendar.listCalendars(success,error);
+
+  // find all events in a named calendar (iOS only for now)
+  window.plugins.calendar.findAllEventsInNamedCalendar(calendarName,success,error);
+
+  // change an event (iOS only for now)
+  var newTitle = "New title!";
+  window.plugins.calendar.modifyEvent(title,location,notes,startDate,endDate,newTitle,location,notes,startDate,endDate,success,error);
+
+  // delete an event (you can pass nulls for irrelevant parameters, note that on Android `notes` is ignored)
+  window.plugins.calendar.deleteEvent(newTitle,location,notes,startDate,endDate,success,error);
